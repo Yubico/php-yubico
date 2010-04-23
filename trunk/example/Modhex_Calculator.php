5a5432354250c607a04471059247a534e3a11b1c
Simon Josefsson <simon@josefsson.org>
2010-04-23 21:33:44 +0000

    Tagging the 2.0 release of the Auth_Yubico project.

---
+++ trunk/example/Modhex_Calculator.php
@@ -0,0 +1,101 @@
+<?php
+/*
+ * Created on May 25, 2009
+ *
+ */
+require_once 'Modhex.php';
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta http-equiv="Cache-Control" content="no-cache, must-revalidate">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+		
+	<style type="text/css">
+	<!--
+	-->
+	</style>
+</head>
+<body onLoad="document.login.srctext.focus();">
+
+	  <div id="stripe">
+	  &nbsp;
+	  </div>
+	  	
+	  <div id="container">
+	  
+		<div id="logoArea">
+           <img src="yubicoLogo.gif" alt="yubicoLogo" width="150" height="75"/>
+		</div>
+		
+		<div id="greenBarContent">
+			<div id="greenBarImage">
+				<img src="yubikey.jpg" alt="yubikey" width="150" height="89"/>
+			</div>
+			<div id="greenBarText">
+				<h3>
+					Modhex Calculator
+				</h3>
+			</div>
+		</div>
+		<div id="bottomContent">		
+<?php
+$srctext = $_REQUEST["srctext"];
+$srcfmt = $_REQUEST["srcfmt"];
+
+if (strlen($srctext) > 0) {
+	
+	if($srcfmt == "O") {
+		$srctext = substr($srctext, 0, 12);
+		$srcfmt = "M";
+	}
+	
+	$b64txt = $srctext;
+	if($srcfmt == "P") {
+		$b64txt = base64_encode($srctext);
+	} else if ($srcfmt == "H") {
+		$hexval = $srctext;
+		$b64txt = hexToB64($hexval);
+		//echo 'Test B64 : '.$b64txt.' :: '.$hexval;
+	} else if ($srcfmt == "M") {
+		$b64txt = modhexToB64($srctext);
+	} else if ($srcfmt == "N") {
+		//$numval = intval($srctext);
+		$numval = gmp_init($srctext);
+		$hexval = gmp_strval($numval,16);
+		//echo 'Test Val : '.$numval.' :: '.$hexval;
+		$b64txt = hexToB64($hexval);
+		//echo 'Test B64 : '.$b64txt;
+	} 
+	
+	$plaintxt = $srctext;
+	//$devId_b64 = modhexToB64($devId);
+	echo '<h2>Your source string is:</h2><ul>'.
+		'<li>Plain text: ' . base64_decode($b64txt) .
+		'<li>Number: ' . gmp_strval(gmp_init(b64ToHex($b64txt),16)) .
+		'<li>Modhex encoded: ' . b64ToModhex($b64txt) .
+		'<li>Base64 encoded: ' . $b64txt .
+		'<li>Hex encoded: ' . b64ToHex($b64txt) . 
+		"</ul>";
+}
+?>
+<p><br>
+
+<form action=Modhex_Calculator.php method=post autocomplete=off>
+	<b>Source format:</b><BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="P">Plain text<BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="N">Number<BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="B">Base64<BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="H">Hex<BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="O" CHECKED>OTP<BR>
+	<INPUT TYPE=RADIO NAME="srcfmt" VALUE="M">Modhex&nbsp;<BR><BR>
+	<b>String:</b>&nbsp;
+	<input name=srctext value="" size=50 maxlength=50><p>
+	<input type=submit value="Convert to all formats">
+</form>
+</div>
+</body>
+</html>
---
+++ trunk/example/debug.php
@@ -0,0 +1,39 @@
+<?php
+	print '<p><table  style="border: 1px solid #e5e5e5; background-color: #f1f1f1; padding: 10px; margin: 0px;" width="100%">';
+
+	print "<tr><td colspan=2><b>HTML fields</b></td></tr>\n";
+	print "<tr><td width=70><b>mode</b></td><td>";
+	if ($username && $password) {
+	    print "two-factor legacy";
+        } else if ($password) {
+	    print "two-factor";
+	} else {
+	    print "one-factor";
+	}
+	print "</td></tr>\n";
+	if ($passwordkey) {
+	    print "<tr><td width=70><b>passwordkey</b></td><td>$passwordkey</td></tr>\n";
+	} else if ($key) {
+	    print "<tr><td width=70><b>key</b></td><td>$key</td></tr>\n";
+	}
+	!$username or
+	 print "<tr><td width=70><b>username</b></td><td>$username</td></tr>\n";
+	!$password or
+	 print "<tr><td width=70><b>password</b></td><td>$password</td></tr>\n";
+	!$identity or
+	 print "<tr><td width=70><b>identity</b></td><td>$identity</td></tr>\n";
+	!$realname or
+	 print "<tr><td width=70><b>db_realname</b></td><td>$realname</td></tr>\n";
+	!$db_password or
+	 print "<tr><td width=70><b>db_password</b></td><td>$db_password</td></tr>\n";
+	!$authenticated or
+	 print "<tr><td width=70><b>rc</b></td><td>$authenticated</td></tr>\n";
+	print "<tr><td colspan=2>&nbsp;</td></tr>\n";
+	print "<tr><td colspan=2><b>Authentication Output</b></td></tr>\n";
+	if ($yubi) {
+	   $txt = $yubi->getLastResponse();
+	}
+	$txt = preg_replace ('/\n/', '<br>', $txt);
+	print "<tr><td colspan=2>$txt</td></tr>\n";
+	print "</table></p>\n";
+?>
